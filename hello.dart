ed73460cff2dd336421784a29d927c3eaa768e4d
0niiChanHentai <trancongminh14042001@gmail.com>
2023-03-19 21:03:53 +0700

    hello

--- hello.dart
+++ hello.dart
@@ -1,4 +1,23 @@
+class Laptop {
+  int id;
+  String name;
+  int ram;
+
+  Laptop({required this.id, required this.name, required this.ram});
+
+  void printDetails() {
+    print("Laptop ID: $id");
+    print("Laptop name: $name");
+    print("Laptop RAM: $ram GB");
+  }
+}
+
 void main() {
-  double a = 2.2222222222222222222;
-  print(a.toStringAsFixed(2));
+  Laptop laptop1 = Laptop(id: 1, name: "Dell Inspiron", ram: 8);
+  Laptop laptop2 = Laptop(id: 2, name: "Lenovo ThinkPad", ram: 16);
+  Laptop laptop3 = Laptop(id: 3, name: "HP Spectre", ram: 32);
+
+  laptop1.printDetails();
+  laptop2.printDetails();
+  laptop3.printDetails();
 }
