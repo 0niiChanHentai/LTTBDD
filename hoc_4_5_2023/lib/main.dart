d98365949c39026911780692a3a71cbaef305b74
0niiChanHentai <trancongminh14042001@gmail.com>
2023-05-09 21:33:46 +0700

    9/5

--- hoc_4_5_2023/lib/main.dart
+++ hoc_4_5_2023/lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(CounterApp());
+}
+
+class CounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        child: Text('Xin chào!'),
+      ),
+    );
+  }
+}
