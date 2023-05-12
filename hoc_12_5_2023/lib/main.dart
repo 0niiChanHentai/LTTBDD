7cbe8d1d57cb3f96ac11710f51bdff7c61158a82
0niiChanHentai <trancongminh14042001@gmail.com>
2023-05-12 23:31:45 +0700

    12/5

--- hoc_12_5_2023/lib/main.dart
+++ hoc_12_5_2023/lib/main.dart
@@ -1,3 +1,6 @@
+// Đây là công thức chung để đưa code vào
+
+/*
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,21 +14,26 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home:
-        Container(
-      constraints: BoxConstraints.expand(
-        height:
-            Theme.of(context).textTheme.headlineMedium!.fontSize! * 1.1 + 200.0,
-      ),
-      padding: const EdgeInsets.all(8.0),
-      color: Colors.blue[600],
-      alignment: Alignment.center,
-      transform: Matrix4.rotationZ(0.1),
-      child: Text('Hello World',
-          style: Theme.of(context)
-              .textTheme
-              .headlineMedium!
-              .copyWith(color: Colors.white)),
-    )
+        
+    );
+  }
+}
+*/
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home:
+        
     );
   }
 }
