b6d3d2699f46dda9801d052bd68f2e462b71080d
0niiChanHentai <trancongminh14042001@gmail.com>
2023-06-19 17:22:19 +0700

    19/06

---
+++ dieu_khien_rviz/android/app/src/main/kotlin/com/example/dieu_khien_rviz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dieu_khien_rviz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- hoc_13_6_2023/android/app/src/main/kotlin/com/example/hoc_13_6_2023/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hoc_13_6_2023
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
