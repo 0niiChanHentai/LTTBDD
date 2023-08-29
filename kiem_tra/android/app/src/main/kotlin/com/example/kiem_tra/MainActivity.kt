a147342945baa86d41f8623dd8c9c7cf6667c321
0niiChanHentai <trancongminh14042001@gmail.com>
2023-08-28 23:22:46 -0700

    28_08_2023

--- kiem_tra/android/app/src/main/kotlin/com/example/kiem_tra/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.kiem_tra
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- kiem_tra_84034/android/app/src/main/kotlin/com/example/kiem_tra_84034/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.kiem_tra_84034
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
