6d555f129c9e54e190bea6fd0b68b86f531b8999
0niiChanHentai <trancongminh14042001@gmail.com>
2023-10-03 23:41:21 +0700

    3_10_2023

---
+++ Đồ_án_Quản_lí_quán_Cà_phê/Web/trang_chu.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <img src="logo.png" class="logo">
+    <div class="tren_phai">
+        <div class="thanh_dieu_huong">
+            <button class="nut1">Trang chủ</button>
+            <button class="nut1">TikTok</button>
+            <button class="nut1">Facebook</button>
+            <button class="nut1">Zalo</button>
+            <button class="nut1">Email</button>
+            <audio controls autoplay>
+                <source src="a.mp3" type="audio/mpeg">
+            </audio>
+            <button class="nut1">Xin chào, Trần Công Minh</button>
+        </div>
+    </div>
+    <div class="duoi">
+        <div class="menu">
+            <button class="nut2">Sản phẩm</button>
+            <button class="nut2">Đơn hàng</button>
+            <button class="nut2">Khách hàng</button>
+            <button class="nut2">Nhân viên</button>
+            <button class="nut2">Tài chính</button>
+            <button class="nut2">Khuyến mãi và giảm giá</button>
+            <button class="nut2">Nguyên liệu</button>
+            <button class="nut2">Bàn và khu vực chỗ ngồi</button>
+            <button class="nut2">Đặt hàng trực tuyến</button>
+            <div class="thanh_ngang"></div>
+            <button class="nut2">Đăng xuất</button>
+        </div>
+        <div class="noi_dung">
+            <!--
+            <video controls autoplay width=80% height=100%>
+                <source src="b.mp4" type="video/mp4">
+            </video>
+            -->
+            <div class="lich">
+                <iframe src="https://calendar.google.com/calendar/embed?src=trancongminh14042001%40gmail.com&ctz=Asia%2FHo_Chi_Minh" style="border: 0" width="100%" height="50%" frameborder="0" scrolling="no"></iframe>
+                <div class="note"></div>
+                <p>Số điện thoại: 0354220664</p>
+                <p>Địa chỉ: Số 6, Lô 5, Khu C2, Cát Bi, Hải An, Hải Phòng</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
