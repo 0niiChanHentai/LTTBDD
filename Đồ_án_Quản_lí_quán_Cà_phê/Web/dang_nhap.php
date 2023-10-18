6a80a16368593b0e83e1152eee335b1768fcd604
0niiChanHentai <trancongminh14042001@gmail.com>
2023-10-19 06:08:43 +0700

    19_10_2023

--- Đồ_án_Quản_lí_quán_Cà_phê/Web/dang_nhap.php
+++ Đồ_án_Quản_lí_quán_Cà_phê/Web/dang_nhap.php
@@ -1,39 +1,36 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="vi">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="dang_nhap.css">
-    <title>Đăng nhập</title>
 </head>
-
 <body>
-
-    <div class="login-container">
-
-        <h2>Đăng nhập</h2>
-
-        <form id="loginForm">
-            <div class="input-group">
-                <label for="username">Tên đăng nhập:</label>
-                <input type="text" id="username" name="username">
-            </div>
-            <div class="input-group">
-                <label for="password">Mật khẩu:</label>
-                <input type="password" id="password" name="password">
-            </div>
-            <div class="input-group">
+    <div class="dang_nhap">
+        <img src="logo.png" alt="Logo" class="logo">
+        
+        <form action="danh_nhap.php" method="post" onsubmit="event.preventDefault(); handleLogin();">
+            <label for="username">Tên đăng nhập:</label>
+            <input type="text" id="username" name="username">
+            
+            <label for="password">Mật khẩu:</label>
+            <input type="password" id="password" name="password">
+            
+            <div class="remember">
                 <input type="checkbox" id="rememberMe" name="rememberMe">
                 <label for="rememberMe">Ghi nhớ mật khẩu</label>
             </div>
-            <button type="button" onclick="handleLogin()">Đăng nhập</button>
+
+            <button type="submit">Đăng nhập</button>
         </form>
-        
+        <a href="#">Quên mật khẩu?</a>
+    </div>
+
+    <div class="anh">
+
     </div>
 
     <script src="dang_nhap.js"></script>
 
 </body>
-
 </html>
